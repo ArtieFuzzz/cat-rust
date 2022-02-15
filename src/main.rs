b242fe0f73dc6b1761c29063e044b6a95553bd33
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-15 11:13:33 +1100

    Conditions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process::exit;
 fn main() {
     let raw_arg = env::args().nth(1);
     let empty = String::from("");
     let arg = raw_arg.unwrap_or(empty);
-    let f = File::open(&arg).expect("Please provide a path to the file!");
-    let reader = BufReader::new(f);
+
+    if arg.is_empty() {
+        println!("{}", "No path provided.");
+        exit(0);
+    }
+
+    let f = File::open(&arg);
+
+    if f.is_err() {
+        println!("{}", "The system cannot find the specified file.");
+        exit(0);
+    }
+
+    let reader = BufReader::new(f.unwrap());
 
     for line in reader.lines() {
         println!("{}", &line.unwrap());
     }
+
+    exit(0);
 }

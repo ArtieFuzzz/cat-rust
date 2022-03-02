5c56910902be1e81097b49ee08f31ded0c95d2f5
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-03-02 20:51:28 +1100

    Match

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,22 @@ use std::io::{BufRead, BufReader};
 use std::process::exit;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let raw_arg = env::args().nth(1);
-    let empty = String::from("");
-    let arg = raw_arg.unwrap_or(empty);
+    let arg = raw_arg.unwrap_or_default();
 
     if arg.is_empty() {
         println!("No path provided.");
         exit(0);
     }
 
-    let f = File::open(&arg);
+    let f = match File::open(&arg) {
+        Ok(f) => f,
+        Err(_) => {
+            println!("Could not find specified file.");
+            exit(0)
+        }
+    };
 
-    if f.is_err() {
-        println!("Cannot find the specified file.");
-        exit(0);
-    }
-
-    let reader = BufReader::new(f?);
+    let reader = BufReader::new(f);
 
     for line in reader.lines() {
         println!("{}", &line?);
